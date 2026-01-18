e499c3b4be3c21670e4b34ab192df412097f9bc4
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-01-18 17:01:54 +0100

    feat: implemented GameObject interface

--- include/GameObject.hpp
+++ include/GameObject.hpp
@@ -1,12 +1,21 @@
-//
-// Created by elti on 12/18/25.
-//
-
 #ifndef TILETWISTER_GAMEOBJECT_H
 #define TILETWISTER_GAMEOBJECT_H
+#include <iostream>
 
 
 class GameObject {
+    public:
+        virtual void setVal(int val) = 0;
+        virtual int getVal() const = 0;
+
+        virtual std::pair<int,int> getPos() const = 0;
+        virtual void setPos(int x, int y) = 0;
+        virtual int getXPos() const = 0;
+        virtual int getYPos() const = 0;
+
+        virtual bool hasMerged() const = 0;
+        virtual void setMerged(bool status) = 0;
+
 };
 
 
